45e7774fb53fc9356abd8a08f0b8b9cb7d653be8
bujin21 <cvj0810@naver.com>
2022-11-03 00:19:31 +0900

    서블릿으로 구구단 구현

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -16,6 +16,6 @@ public class HelloServlet extends HttpServlet {
     resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-8로 하겠다.
     resp.setContentType("text/html; charset-utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8이라고 알리는 의미
 
-    resp.getWriter().append("안녕하세요.");
+    resp.getWriter().append("반갑습니다.1111111");
   }
 }
