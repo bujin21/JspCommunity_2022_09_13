c54f63a8d1d6fbe1b2d649e028ff2a60cd920130
bujin21 <cvj0810@naver.com>
2022-11-08 00:19:27 +0900

    게시물 작성시 로그인 된 회원의 번호가 article 테이블에 같이 저장되도록 작업

--- db.sql
+++ db.sql
@@ -43,4 +43,42 @@ CREATE TABLE `member` (
 	`name` CHAR(100) NOT NULL
 );
 
+# 회원 테이블 데이터 생성
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'admin',
+loginPw = 'admin',
+`name` = '관리자';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user1',
+loginPw = 'user1',
+`name` = 'user1';
+
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user2',
+loginPw = 'user2',
+`name` = '유저2';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'user3',
+loginPw = 'user3',
+`name` = '유저3';
+
 SELECT * FROM `member`;
+
+#게시물 테이블에 memberId 칼럼 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+#기존 게시물은 그냥 2번 회원이 작성한걸로 한다.
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
--- src/main/java/com/sbs/exam/servlet/ArticleDoWriteServlet.java
+++ src/main/java/com/sbs/exam/servlet/ArticleDoWriteServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -23,6 +24,14 @@ public class ArticleDoWriteServlet extends HttpServlet {
     req.setCharacterEncoding("UTF-8");
     resp.setCharacterEncoding("UTF-8");
     resp.setContentType("text/html; charset-utf-8");
+
+    HttpSession session = req.getSession();
+
+    if( session.getAttribute("loginedMemberId") == null){
+      resp.getWriter().append(String.format("<script> alert('로그인 후 이용해주세요.'); location.replace('../member/login'); </script>"));
+
+      return;
+    }
 
     String driverName = Config.getDriverClassName();
 
@@ -44,11 +53,14 @@ public class ArticleDoWriteServlet extends HttpServlet {
       String title = req.getParameter("title");
       String body = req.getParameter("body");
 
+      int loginedMemberId = (int) session.getAttribute("loginedMemberId");
+
       SecSql sql = SecSql.from("INSERT INTO article");
       sql.append("SET regDate = NOW()");
       sql.append(", updateDate = NOW()");
       sql.append(", title = ?", title);
       sql.append(", body = ?", body);
+      sql.append(", memberId = ?", loginedMemberId);
 
       int id = DBUtil.insert(con, sql);
       resp.getWriter().append(String.format("<script> alert('%d번 글이 등록되었습니다.'); location.replace('list'); </script>", id));
