774dc359c6eb6910ba97e0d0c9f0f86e34116eef
bujin21 <cvj0810@naver.com>
2022-11-07 23:11:02 +0900

    로그인, 로그아웃 기능 구현

--- src/main/java/com/sbs/exam/servlet/MemberDoLogoutServlet.java
+++ src/main/java/com/sbs/exam/servlet/MemberDoLogoutServlet.java
@@ -1,5 +1,9 @@
 package com.sbs.exam.servlet;
 
+import com.sbs.exam.Config;
+import com.sbs.exam.exception.SQLErrorException;
+import com.sbs.exam.util.DBUtil;
+import com.sbs.exam.util.SecSql;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,29 +12,28 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
 
-@WebServlet("/home/main")
-public class HomeMainServlet extends HttpServlet {
+@WebServlet("/member/doLogout")
+public class MemberDoLogoutServlet extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     req.setCharacterEncoding("UTF-8");
     resp.setCharacterEncoding("UTF-8");
     resp.setContentType("text/html; charset-utf-8");
 
+
+
     HttpSession session = req.getSession();
+     session.removeAttribute("loginedMemberId");
 
-    boolean isLogined = false;
-    int loginedMemberId = -1;
+     resp.getWriter().append(String.format("<script> alert('로그아웃'); location.replace('../home/main'); </script>"));
 
-    if(session.getAttribute("loginedMemberId") != null){
-      loginedMemberId = (int) session.getAttribute("loginedMemberId");
-      isLogined = true;
-    }
 
-    req.setAttribute("isLogined", isLogined);
-    req.setAttribute("loginedMemberId", loginedMemberId);
-
-    req.getRequestDispatcher("../home/main.jsp").forward(req, resp);
   }
 
   @Override
--- src/webapp/home/main.jsp
+++ src/webapp/home/main.jsp
@@ -1,13 +1,32 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 
+<%
+boolean isLogined = (boolean) request.getAttribute("isLogined");
+int loginedMemberId = (int) request.getAttribute("loginedMemberId");
+%>
+
 <!doctype html>
 <head>
   <meta charset="UTF-8">
   <title>메인</title>
 </head>
 <body>
-<h1>메인 페이지</h1>
-<div>
+  <h1>메인 페이지</h1>
+
+  <% if ( isLogined ) { %>
+  <div>
+    <%=loginedMemberId%>번 회원님 환영합니다.
+    <a href="../member/doLogout">로그아웃</a>
+  </div>
+  <% } %>
+
+  <% if ( !isLogined ) { %>
+  <div>
+    <a href="../member/login">로그인</a>
+  </div>
+  <% } %>
+
+  <div>
   <a href="../article/list">게시물 리스트</a>
 </div>
 </body>
